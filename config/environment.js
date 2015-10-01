77b917c2295c65186aadbb19bc99da9d9ec9d755
nataliyamiller <nataliyamiller82@gmail.com>
2015-10-01 00:10:36 -0700

    Some routes fixed for rentals

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'super-rental',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://super-rentals-rental.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
